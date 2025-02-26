e5b723adcde581c94b19cc504a6fbd9434675082
Sebastian Serruto <sebastianserruto@gmail.com>
2025-02-26 18:03:14 -0300

    Creacion de Modulo Admin y coneccion a BD

---
+++ admin/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+    require '../includes/funciones.php';
+    incluirTemplate('header');
+?>
+
+    <main class="contenedor seccion">
+        <h1>Administrador de Bienes Raices</h1>
+        
+        <a class="boton boton-verde" href="/admin/propiedades/crear.php">Nueva Propiedad</a>
+    </main>
+
+<?php incluirTemplate('footer') ?>
